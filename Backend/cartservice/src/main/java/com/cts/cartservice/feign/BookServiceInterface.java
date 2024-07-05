dce81911c3992b1a96d4a2cc7de092e3d71b4123
adityakammari19 <adityakammari@gmail.com>
2024-07-05 13:30:43 +0530

    added feign annotation in cart service

--- Backend/cartservice/src/main/java/com/cts/cartservice/feign/BookServiceInterface.java
+++ Backend/cartservice/src/main/java/com/cts/cartservice/feign/BookServiceInterface.java
@@ -1,11 +1,13 @@
 package com.cts.cartservice.feign;
 
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cts.cartservice.dto.BookDTO;
 
+@FeignClient(name = "book-service")
 public interface BookServiceInterface {
 	
 	@GetMapping("/books/{bookId}")
